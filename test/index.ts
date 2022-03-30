6cad460fa4d40f01e04ad522f0aca61d23139807
Joe Wagner <njwjs722@gmail.com>
2022-03-30 09:50:05 -0600

    WIP: looking at issue #3

--- test/index.ts
+++ test/index.ts
@@ -16,6 +16,8 @@ dotenv.config({path: "../"});
 const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
 
 interface Network {
+  name: string;
+  overrides?: any;
   contract: string;
   rpc: string;
 };
@@ -35,14 +37,21 @@ const testFactory = async function (network: Network) {
 
   const contract = new ethers.Contract(network.contract, artifact.abi, wallet);
 
-  const transactionResponse = await contract.storeData("fake_table_name", query);
+  const args = network.overrides ? ["fake_table_name", query, network.overrides] : ["fake_table_name", query];
+
+  const estimateGasResponse = await contract.estimateGas.storeData.apply(contract.estimateGas, args);
+  console.log(`${network.name} Gas Estimate: ${estimateGasResponse.toString()}`);
+
+  /*const transactionResponse = await contract.storeData.apply(contract, args);
 
   const result = await transactionResponse.wait();
 
   // TODO: add a logger that saves this data
-  console.log(result);
+  //console.log(result);
+  console.log(`${network.name} Gas Used: ${result.gasUsed.toString()}`);
 
   await expect(transactionResponse).to.emit(contract, "DataStored").withArgs("fake_table_name", query);
+  */
 };
 
 describe("Registry", function () {
@@ -53,6 +62,7 @@ describe("Registry", function () {
   it("log details of transaction on Arbitrum", async function () {
     await testFactory({
       // Arbitrum Rinkeby Testnet
+      name: "Arbitrum",
       contract: "0xB72ee475aB153De39bdD2A3c50508Ab8920AFdD7",
       rpc: "https://rinkeby.arbitrum.io/rpc"
     });
@@ -61,6 +71,7 @@ describe("Registry", function () {
   it("log details of transaction on Optimism", async function () {
     await testFactory({
       // Optimism Rinkeby Testnet
+      name: "Optimism",
       contract: "0x23C5e9D53CBAf6703839A503f0429C5D01796858",
       rpc: "https://kovan.optimism.io/"
     });
@@ -69,14 +80,17 @@ describe("Registry", function () {
   it("log details of transaction on Polygon", async function () {
     await testFactory({
       // Polygon Rinkeby Testnet
+      name: "Polygon",
+      //overrides: {gasPrice: 30, gasLimit: 100},
       contract: "0xB72ee475aB153De39bdD2A3c50508Ab8920AFdD7",
-      rpc: "https://matic-mumbai.chainstacklabs.com"
+      rpc: "https://matic-mumbai.chainstacklabs.com"//`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
     });
   });
 
   it("log details of transaction on Avalanche", async function () {
     await testFactory({
       // Avalanche Rinkeby Testnet
+      name: "Avalanche",
       contract: "0xB72ee475aB153De39bdD2A3c50508Ab8920AFdD7",
       rpc: "https://api.avax-test.network/ext/bc/C/rpc"
     });
